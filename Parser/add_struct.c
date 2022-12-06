7fe31b3fbbe9df8260377a72f0acf073efc552f9
Kaano1 <ahmetyumusakdiken13@gmail.com>
2022-12-06 07:16:27 +0300

    save after a few hour

--- Parser/add_struct.c
+++ Parser/add_struct.c
@@ -57,7 +57,7 @@ char	*ft_add_string(t_shell *mini, int index, int start) //ahmet -d < "ceren" < 
 	int	j;
 	int	i;
 
-	str = malloc(sizeof(char *) * (len_word(mini->parse[index], 32) - start) + 1);
+	str = ft_calloc(sizeof(char *), (len_word(mini->parse[index], 32) - start) + 1);
 	j = 0;
 	i = 0;
 	while (start <= len_word(mini->parse[index], 32))
